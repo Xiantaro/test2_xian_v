a98e6edf3d62704e32bd78e431c8ba7e9c75c861
Xiann <ns45665412@gmail.com>
2025-07-24 19:12:43 +0800

    udpate

--- Models/ManagementModels/ZhongXian/Appoimtment/AppointmentModelSendClass.cs
+++ Models/ManagementModels/ZhongXian/Appoimtment/AppointmentModelSendClass.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using test2.Models.ManagementModels.GmailSMTP;
 using test2.Models.ManagementModels.ZhongXian.Normal;
@@ -15,27 +16,30 @@ namespace test2.Models.ManagementModels.ZhongXian.Appoimtment
         }
         public async Task<ResultViewModel> AppointmentSend(int appointmentMode_UserID, int appointmentMode_BookId)
         {
+            Debug.WriteLine("ID: " + appointmentMode_UserID);
             var User = await _context.Clients.Where(x => x.CId == appointmentMode_UserID).ToListAsync();
-            if (User.IsNullOrEmpty()) { return new ResultViewModel() { ResultCode = 0, Message= "該名借閱者不存在，請重新輸入" }; }
+            Debug.WriteLine("User: " + User.Count);
+            if (User?.Count == 0) { return new ResultViewModel() { ResultCode = 0, Message= "該名借閱者不存在，請重新輸入" }; }
+            Debug.WriteLine("下去了");
             var bookId = await _context.Collections.Where(x => x.CollectionId == appointmentMode_BookId).Select(re => new { re.Title }).ToListAsync();
-            if (bookId.IsNullOrEmpty()) { return new ResultViewModel() { ResultCode = 0, Message = "該本書籍不存在，請重新輸入" }; }
+            if (bookId?.Count == 0) { return new ResultViewModel() { ResultCode = 0, Message = "該本書籍不存在，請重新輸入" }; }
             var ResultMessage = await _context.Set<MessageDTO>().FromSqlInterpolated($"EXEC AppointmentMode {appointmentMode_UserID}, {appointmentMode_BookId}").ToListAsync();
             var result = new ResultViewModel()
             {
                 ResultCode = ResultMessage[0].ResultCode,
                 Message = ResultMessage[0].Message,
                 Cid = appointmentMode_UserID,
-                cName = User[0].CName,
-                title = bookId[0].Title,
+                cName = User?[0].CName,
+                title = bookId?[0].Title,
             };
             //Email發送
-            if (result.ResultCode == 1)
-            {
-                string subject = "【預約成功通知】";
-                string body = $"親愛的 {User[0].CName}，您已於 {DateTime.Now} \r\n預約了《{bookId[0].Title}》請耐心等候通知。\r\n圖書館管理系統 敬上。";
-                string email = "ns45665412@gmail.com";
-                await EmailSenders.SendAsync(email, subject, body);
-            }
+            //if (result.ResultCode == 1)
+            //{
+            //    string subject = "【預約成功通知】";
+            //    string body = $"親愛的 {User[0].CName}，您已於 {DateTime.Now} \r\n預約了《{bookId[0].Title}》請耐心等候通知。\r\n圖書館管理系統 敬上。";
+            //    string email = "ns45665412@gmail.com";
+            //    await EmailSenders.SendAsync(email, subject, body);
+            //}
             return result;
         }
     }
